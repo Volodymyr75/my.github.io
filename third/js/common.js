3c929800da3e081f0ee8e8305fe3a40e0aff3d6a
Volodymyr75 <homer.strem@gmail.com>
2017-10-11 12:01:00 +0300

    add effects

--- third/js/common.js
+++ third/js/common.js
@@ -74,7 +74,7 @@ $(window).load(function() {
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
 
-	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
-	$(".top_text p").animated("fadeInUp", "fadeOutDown");
+	$(".page-header__logo").animated("fadeInDown", "fadeOutUp");
+	$(".page-header__tagline").animated("fadeInUp", "fadeOutDown");
 
 });
